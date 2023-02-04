d7cd3d7349c88435ab003258e4e09f50a312c8ad
Piotr <lis.piotr121@gmail.com>
2023-02-04 01:56:16 +0100

    Added README to the ABOUT page

--- about.php
+++ about.php
@@ -2,5 +2,5 @@
 
 <h2>About</h2>
 
-<p class="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore impedit totam porro iure reiciendis autem possimus sapiente, optio, exercitationem ipsum assumenda mollitia, recusandae expedita culpa ratione voluptatem esse quos quam?</p>
+<p class="text-center">Basic PHP project with a little bit of MySQL. It's a feedback website with 3 pages. 1st one with a form where you need to provide name, email and feedback which are then added to a database. In case either of those values is not provided an adequat error will show up. 2nd one where you can check every feedback that was submited to the database. The 3rd one is the "About" section.</p>
 <?php include 'inc/footer.php'; ?>
